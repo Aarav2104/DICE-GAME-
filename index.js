fcf000d6cbd3d74ee44fd8e46fa20129a31b839e
Aarav 21 <aaravkg21@gmail.com>
2025-12-26 00:04:33 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+function rollDice(){
+var randomNumber1=Math.random();
+randomNumber1=randomNumber1*6;
+randomNumber1=Math.floor(randomNumber1)+1;
+console.log(randomNumber1);
+document.querySelector(".img1").setAttribute("src","./images2/dice"+randomNumber1+".png");
+
+var randomNumber2=Math.random();
+randomNumber2=randomNumber2*6;
+randomNumber2=Math.floor(randomNumber2)+1;
+console.log(randomNumber2);
+document.querySelector(".img2").setAttribute("src","./images2/dice"+randomNumber2+".png");
+
+if(randomNumber1==randomNumber2){
+    document.querySelector("h1").innerHTML="Draw";
+}
+else if(randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML="Player 1 Win";
+}
+else if(randomNumber1<randomNumber2){
+    document.querySelector("h1").innerHTML="Player 2 Win";
+}
+}
